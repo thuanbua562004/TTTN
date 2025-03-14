20bd3a51cf7284917442afce4eaf519e05698243
thuanbua562004 <157040478+thuanbua562004@users.noreply.github.com>
2025-03-14 17:59:06 +0700

    Update config.js

--- src/AxiosConfig/config.js
+++ src/AxiosConfig/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost:3000", // Đổi theo backend của bạn
+  baseURL: "https://tttnserver.onrender.com", // Đổi theo backend của bạn
   headers: { "Content-Type": "application/json" },
 });
 
